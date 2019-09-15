a37742bc16d361188cd47ee906c881d828d26cd3
Yigit Dallilar <yigit.dallilar@gmail.com>
2019-09-15 15:08:38 +0300

    pretty_os_release for systemd-boot and OSRELEASE_FILE, INITRD_FILE variables

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,6 +8,9 @@ CMDLINE_DIR=$BOOTDIR
 UCODE=$BOOTDIR/intel-ucode.img
 EFISTUB=/usr/lib/systemd/boot/efi/linuxx64.efi.stub
 
+OSRELEASE_FILE=/tmp/arch-efiboot/os-release 
+INITRD_FILE=/tmp/arch-efiboot/initrd.bin 
+
 find_kernels () {
 
 	KERNELS=$(ls /boot/vmlinuz-* | xargs -n1 basename | sed "s/vmlinuz-//")
@@ -15,15 +18,15 @@ find_kernels () {
 
 }
 
-add_ucode () {
+make_initrd () {
+
+	INITRD="$BOOTDIR/initramfs-$KERNEL.img"
 
 	if [ -f "$UCODE" ]; then
-		cat "$UCODE" "$INITRD" > /tmp/initrd.bin
-		INITRDFILE=/tmp/initrd.bin
+		cat "$UCODE" "$INITRD" > $INITRD_FILE
 	else
-		# Do not fail on AMD systems
-		echo "    Intel microcode not found. Skipping."
-		INITRDFILE="$INITRD"
+		echo "    Intel microcode not found."
+		cat "$INITRD" > $INITRD_FILE
 	fi
 
 }
@@ -44,16 +47,27 @@ set_cmdline () {
 
 }
 
+pretty_os_release () {
+
+	cp /usr/lib/os-release $OSRELEASE_FILE
+	KERNEL_VERSION=$(pacman -Qi $KERNEL | grep Version | cut -d: -f2 | xargs)
+	sed -i "s/BUILD_ID=rolling/BUILD_ID=$KERNEL_VERSION/" $OSRELEASE_FILE
+	sed -i "s/ID=arch/ID=$KERNEL/" $OSRELEASE_FILE
+
+}
+
 make_efi_kernel () {
 
 	objcopy \
-	    --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
+	    --add-section .osrel=$OSRELEASE_FILE --change-section-vma .osrel=0x20000 \
 	    --add-section .cmdline="$CMDLINE" --change-section-vma .cmdline=0x30000 \
 	    --add-section .linux="$BOOTDIR/vmlinuz-$KERNEL" --change-section-vma .linux=0x40000 \
-	    --add-section .initrd="$INITRDFILE" --change-section-vma .initrd=0x3000000 \
+	    --add-section .initrd=$INITRD_FILE --change-section-vma .initrd=0x3000000 \
 	    "$EFISTUB" "$TARGET/$KERNEL.efi"
 
 }
+
+mkdir -p /tmp/arch-efiboot
 
 find_kernels
 
@@ -62,10 +76,13 @@ echo "Updating EFI kernels..."
 for KERNEL in $KERNELS; do
 
 	echo "  Building $KERNEL"
-	INITRD="$BOOTDIR/initramfs-$KERNEL.img"
 
-	add_ucode
+	make_initrd
 	set_cmdline
+	pretty_os_release
 	make_efi_kernel
 
 done
+
+rm -r /tmp/arch-efiboot
+
